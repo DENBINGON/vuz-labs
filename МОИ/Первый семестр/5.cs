2c2fe09746678f313ca0c2aeb8c508381d8e5dc0
Ruslan <35464027+DENBINGON@users.noreply.github.com>
2021-10-17 21:16:38 +0300

    мои 5-6 лаба

---
+++ МОИ/Первый семестр/5.cs
@@ -0,0 +1,22 @@
+using System;
+namespace q
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЛР МОИ №5 задание №4
+
+
+            //Console.Write($"long min = {long.MinValue}\nlong max = {long.MaxValue}\n" +
+            //    $"ulong min = {ulong.MinValue}\nulong max = {ulong.MaxValue}");
+
+
+            //ЛР МОИ №5 задание №3
+
+            //byte SB = 0b11001011;
+            //Console.WriteLine($"11001011 \nbyte -> {SB}\nsbyte -> {byte.MaxValue - ((sbyte) ~SB)}");
+
+            //ushort UB = 0b1111111100000000;
+            //Console.WriteLine($"1111111100000000\nuint16 -> {UB}\nint16 -> {ushort.MaxValue - ((short) ~UB)}");
+}}}
---
+++ МОИ/Первый семестр/6.cs
@@ -0,0 +1,16 @@
+using System;
+namespace q
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЛР МОИ №6 задание №2
+
+
+            //Console.WriteLine("Введите первое число");
+            //byte first = Convert.ToByte(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //byte second = Convert.ToByte(Console.ReadLine());
+            //Console.Write("Результат: " + (byte)(first + ((byte) ~second+1)));
+}}}
