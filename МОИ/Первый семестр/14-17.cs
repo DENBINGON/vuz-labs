def1520569b3345aa29f43a27d0d717f8be9ae70
Ruslan <35464027+DENBINGON@users.noreply.github.com>
2021-12-15 09:51:58 +0300

    9-17

---
+++ МОИ/Первый семестр/14-17.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+namespace МОИ14_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЛР МОИ 14.1
+            //UInt32 Q = UInt32.MaxValue - 3339;
+            //UInt32 R = Q;
+            //for (int i = 1; i < 32; i++)
+            //{
+            //    UInt32 L = Q;
+            //    for (int e = 0; e < i; e++)
+            //    {
+            //        L = L >> 1;
+            //    }
+            //    R -= L;
+            //}
+            //Console.WriteLine(Convert.ToString(R));
+            //int q = 0;
+            //R = Q;
+            //for (int i = 0; i<32; i++)
+            //{
+            //    if ((byte) (R & 1) == 1) q++;
+            //    R >>= 1;
+            //}
+            //Console.WriteLine(q);
+
+            //ЛР МОИ 14.2
+
+            //UInt32 Q = uint.MaxValue - 3339;
+            //UInt32 R = Q;
+            //UInt32 o = 0;
+            //Console.WriteLine(R);
+            //while (true)
+            //{
+            //    o++;
+            //    try
+            //    {
+            //        UInt32 s = Q / Convert.ToUInt32(Math.Pow(10, o));
+            //        R -= s*9;
+            //    }
+            //    catch { break;  }
+            //}
+            //Console.WriteLine(R);
+            //int r = 0;
+            //string q = Convert.ToString(Q);
+            //for (int i = 0; i < q.Length; i++) r += Convert.ToInt16(q[i]) - 48;
+            //Console.WriteLine(r);
+
+            //ЛР МОИ 14.3
+
+            //int l = 453;
+            //List<int> arr = new List<int>();
+            //Random r = new Random();
+            //for (int i = 0; i < l; i++) arr.Add(r.Next(0, 2));
+            //arr.Sort();
+            //int b = 0;
+            //while (true)
+            //{
+            //    if (arr[b] == 1) break;
+            //    b += 1;
+            //}
+            //if (b > l - b) Console.WriteLine("Нулей больше");
+            //else Console.WriteLine("Единиц больше");
+
+            //ЛР МОИ 14.4
+
+            //BigInteger integer = 1;
+            //int k = 999;
+            //for (int i = 1; i<k+1; i++)
+            //{
+            //    integer *= i;
+            //}
+            //Console.WriteLine(integer);
+
+            //ЛР МОИ 15.2
+
+            //int[] k = { 14, 18 };
+            //Console.WriteLine(NOD(k));
+
+            //ЛР МОИ 16.1
+
+            List<int> p = GetUsual(1, 9999);
+            Console.WriteLine(String.Join(" ", p));
+
+        }
+
+        static int NOD(int[] w)
+        {
+            while (w[1] != 0)
+            {
+                int k = w[0];
+                w[0] = w[1];
+                w[1] = k % w[0];
+            }
+            return w[0];
+        }
+
+        static bool Usual(UInt32 num)
+        {
+            for (int i = 2; i < num; i++) if (num % i == 0) return false;
+            return true;
+        }
+        static List<int> GetUsual(int a, int b)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = a + 1; i < b; i++) if (Usual(Convert.ToUInt32(i))) list.Add(i);
+
+            return list;
+        }
+    }
+}
---
+++ МОИ/Первый семестр/9-13.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace ___
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЛР МОИ №9.3
+
+            //Console.WriteLine("Число:");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine((num << 5) + (num << 3));
+
+            //ЛР МОИ №9.4
+
+            //Console.WriteLine("Число:");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{num >> 4}.{num & 15}");
+
+            //ЛР МОИ №13.а.4
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ////win1251
+            //byte[] b = { 0xD1, 0xEE, 0xEB, 0xEE, 0xE2, 0xFC, 0xE5, 0xE2, 0x0D, 0x0A };
+            ////866
+            ////byte[] b = { 0x91, 0xAE, 0xAB, 0xAE, 0xA2, 0x9C, 0xF1, 0xA2, 0x0D, 0x0A };
+            ////koi8 - r 20866
+            ////byte[] b = { 0xF3, 0xCF, 0xCC, 0xCF, 0xD7, 0xD8, 0xC5, 0xD7, 0x0D, 0x0A };
+            //Encoding enc = Encoding.GetEncoding(1251);
+            //BinaryWriter binout = new BinaryWriter(new FileStream("../../../a.bin", FileMode.Create), enc);
+            //binout.Write(b);
+            //binout.Close();
+
+            //ЛР МОИ №13.б.4
+
+            //utf - 8
+            //byte[] b = { 0xEF, 0xBB, 0xBF, 0xD0, 0xA1, 0xD0, 0xBE };
+            //utf - 16le
+            //byte[] b = { 0xFF, 0xFE, 0x21, 0x04, 0x3E, 0x04 };
+            //utf - 32le
+            //byte[] b = { 0xFF, 0xFE, 0x00, 0x00, 0x21, 0x04, 0x00, 0x00, 0x3E, 0x04, 0x00, 0x00 };
+            //BinaryWriter binout = new BinaryWriter(new FileStream("../../../b.bin", FileMode.Create));
+            //binout.Write(b);
+            //binout.Close();
+
+            //ЛР МОИ №10.доп
+
+            //byte num = 0b11010101;
+            //byte num_s = (byte)(num >> 4);
+            //byte num_m = (byte)((byte)(num << 4) >> 4);
+            //double sum = 0;
+            //byte k = 0;
+            //bool l = true;
+            //for (byte i = 0; i < 4; i++)
+            //{
+            //    if ((byte)((byte)(num_s << 4 + i) >> 7) == 1)
+            //    {
+            //        l = false;
+            //        sum += Math.Pow(2, i - k);
+            //    }
+            //    else if (l) k++;
+
+            //    if ((byte)((byte)(num_m << 4 + i) >> 7) == 1)
+            //    {
+            //        sum += 1 / Math.Pow(2, i + 1);
+            //    }
+            //}
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BinaryReader b = new BinaryReader(new FileStream("../../../a.bin", FileMode.Open), Encoding.GetEncoding(1251));
+//Console.WriteLine(Convert.ToString(b.Read(), 2));
