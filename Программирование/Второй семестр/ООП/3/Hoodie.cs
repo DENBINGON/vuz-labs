6331931e6a71c1ee6ff98e1fd94ab61fcdb49ac1
Ruslan <35464027+DENBINGON@users.noreply.github.com>
2022-02-26 07:53:54 +0300

    ооп 3

---
+++ Программирование/Второй семестр/ООП/3/Hoodie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace oop_3
+{
+    class Hoodie : ProductEnt
+    {
+        private int hoodieSize;
+        private bool hoodieCap;
+        private string hoodieGender;
+
+        public Hoodie(int cost = 0, int size = 0,
+            bool cap = false, string gender = "Unknow")
+        {
+            productName = "Толстовка";
+            productCost = cost;
+            hoodieCap = cap;
+            hoodieGender = gender;
+            hoodieSize = size;
+        }
+
+        public new string GetItem()
+        {
+            return $"ID: {productId}\nНаименование: {productName}\n" +
+                $"Размер: {hoodieSize}\nПол: {hoodieGender}\n" +
+                $"Капюшон: {hoodieCap}\nСтоимость: {productCost}\n";
+        }
+
+        public string GetItem(string key)
+        {
+            key = key.ToLower();
+            switch (key)
+            {
+                case "id": return Convert.ToString(productId);
+                case "size": return Convert.ToString(hoodieSize);
+                default: return "Invalid key";
+            }
+        }
+
+        public override void AddItem()
+        {
+            basket.Add(new Hoodie(productCost, hoodieSize, hoodieCap, hoodieGender));
+        }
+    }
+}
---
+++ Программирование/Второй семестр/ООП/3/ProductEnt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+namespace oop_3
+{
+    abstract class ProductEnt
+    {
+        protected static List<ProductEnt> basket = new List<ProductEnt>();
+        protected string productName = "Unknow" ;
+        protected static int counter = 0;
+        protected int productId;
+        protected int productCost = 0;
+     
+        protected ProductEnt()
+        {
+            productId = counter++;
+        }
+
+        public abstract void AddItem();
+
+        public string GetItem()
+        {
+            return $"ID: {productId}\nНаименование: {productName}\n" +
+                $"Стоимость: {productCost}\n";
+        }
+
+        public int GetOverCost()
+        {
+            int con = 0;
+            foreach (ProductEnt obj in basket)
+            {
+                con += obj.productCost;
+            }
+            return con;
+        }
+    }
+}
+//protected ProductEnt(string productName, int productId, int productCost)
+//{
+//    this.productName = productName;
+//    this.productId = productId;
+//    this.productCost = productCost;
+//}
+//public void AddItem ()
+//{
+//    basket.Add(new ProductEnt(productName, productId, productCost));
+//}
