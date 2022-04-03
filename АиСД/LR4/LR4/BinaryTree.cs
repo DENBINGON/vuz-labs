861570e8e921ed75ab9ba49113322e7a4fbd5a96
Ruslan <denbingon@gmail.com>
2022-04-03 20:36:26 +0300

    аисд 4

---
+++ АиСД/LR4/LR4/BinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace LR4
+{
+    public class BinaryNode
+    {
+        public BinaryNode left { get; set; }
+        public BinaryNode right { get; set; }
+        
+        public int value;
+
+        public BinaryNode(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        private BinaryNode root;
+
+        public BinaryTree(int value)
+        {
+            root = new BinaryNode(value);
+        }
+        
+        public void Add(int value) => Add_(value, root);
+        
+        public bool Find(int value) => Find_(value, root);
+
+        public void AddIfNotExist(int value)
+        {
+            if (!Find(value)) Add(value);
+        }
+        
+        private void Add_(int value, BinaryNode node)
+        {
+            if (value > node.value)
+            {
+                if (node.right == null)
+                {
+                    node.right = new BinaryNode(value);
+                }
+                else
+                {
+                    Add_(value, node.right);
+                }
+            } 
+            else if (value < node.value)
+            {
+                if (node.left == null)
+                {
+                    node.left = new BinaryNode(value);
+                }
+                else
+                {
+                    Add_(value, node.left);
+                }
+            }
+        }
+        
+        private bool Find_(int value, BinaryNode node)
+        {
+            if (node.value == value) return true;
+            else if (node.value < value)
+            {
+                if (node.right == null) return false;
+                else if (node.right.value == value) return true;
+                else
+                {
+                    return Find_(value, node.right);
+                }
+            }
+            else
+            {
+                if (node.left == null) return false;
+                else if (node.left.value == value) return true;
+                else
+                {
+                    return Find_(value, node.left);
+                }
+            }
+        }
+
+    }
+}
--- АиСД/LR4/LR4/Program.cs
+++ АиСД/LR4/LR4/Program.cs
@@ -1,16 +1,26 @@
-﻿namespace LR4
+﻿using System;
+
+namespace LR4
 {
     
     /*5. Описать процедуру или функцию, которая:
       a) определяет, входит ли вершина с записью Е в дерево Т;
-      b) если такая запись не найдена, то она добавляется.
-    */
+      b) если такая запись не найдена, то она добавляется.*/
     
     internal class Program
     {
         public static void Main(string[] args)
         {
-            BinaryTree
+            BinaryTree bt = new BinaryTree(50);
+            bt.Add(45);
+            bt.Add(55);
+            bt.Add(42);
+            bt.Add(46);
+            bt.Add(57);
+            bt.Add(53);
+            Console.WriteLine(bt.Find(58));
+            bt.AddIfNotExist(58);
+            Console.WriteLine(bt.Find(58));
         }
     }
 }
